ba2519543f6f0935402dc0e4b191fc4cd3254d33
Biitez <biitecito@gmail.com>
2021-11-28 03:25:47 -0300

    adding a few unimportant things

---
+++ CapSharp/Enums/ProxyProtocol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapSharp.Enums
+{
+    internal enum ProxyProtocol
+    {
+        SOCKS4,
+        SOCKS5,
+        HTTP,
+        HTTPS
+    }
+}
---
+++ CapSharp/Models/Proxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapSharp.Models
+{
+    internal class Proxy
+    {
+        public string IpAddress { get; set; }
+
+        public int Port { get { return _Port; } 
+            set 
+            { 
+                if (value > 65535) throw new ArgumentOutOfRangeException(nameof(Port));
+
+                _Port = value;
+            }
+        }
+
+        public int _Port;
+    }
+}
